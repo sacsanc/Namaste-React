7363dd6d58d171adee8cd50e982731742ec557a5
Sachin Chaudhari <chaudhari.sach22@gmail.com>
2024-01-28 21:07:51 +0530

    Namaste react first commit

---
+++ namaste-react/WebContent/my-app/App.js
@@ -0,0 +1,7 @@
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1 tag!!")));
+
+console.log(parent);
+const heading = React.createElement("h1",{id:"heading"},"Hello World! from react from App.js!!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+///root.render(heading);
+root.render(parent);

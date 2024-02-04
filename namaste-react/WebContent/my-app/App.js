a4cc3460c5927974738b3f1baae4bb8e94bcb4d6
Sachin Chaudhari <chaudhari.sach22@gmail.com>
2024-02-04 22:57:56 +0530

    Igniting the app

--- namaste-react/WebContent/my-app/App.js
+++ namaste-react/WebContent/my-app/App.js
@@ -1,4 +1,7 @@
-const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1 tag!!")));
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"I am h1h1 tag!!")));
 
 console.log(parent);
 const heading = React.createElement("h1",{id:"heading"},"Hello World! from react from App.js!!!");
